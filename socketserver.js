58102dc66441fcc9181c201d889a5673ec6ad07d
Ted de Koning <teddk23@gmail.com>
2012-01-02 17:48:14 +0100

    node-static logica implementeren

--- socketserver.js
+++ socketserver.js
@@ -62,3 +62,21 @@ io.sockets.on('connection', function (socket) {
 		this.name = 'Anonymous';
 	}
 });
+
+
+// http server
+var static = require('node-static');
+
+//
+// Create a node-static server instance to serve the './clients' folder
+//
+var file = new(static.Server)('./');
+
+require('http').createServer(function (request, response) {
+	request.addListener('end', function () {
+		//
+		// Serve files!
+		//
+		file.serve(request, response);
+	});
+}).listen(1337);
