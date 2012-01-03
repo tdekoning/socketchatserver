9fae3c86ae1b9430615bf5170935599cacb7df13
Ted de Koning <teddk23@gmail.com>
2012-01-03 18:23:20 +0100

    chatlogic logica verplaatsen naar apart bestand

--- socketserver.js
+++ socketserver.js
@@ -1,71 +1,10 @@
 var io = require('socket.io').listen(5000),
-	people = [];
+	chatlogic = require('./chatlogic.js'),
+	static = require('node-static');
 
-io.sockets.on('connection', function (socket) {
-	// Create a new Person object for the given socket
-	people.push( new Person(socket) );
-
-	/*
-	 * Joinlistener, adds additional data to the current person and tells everyone that he/she joined
-	 */
-	socket.on('join', function( data ) {
-		var person = people[findIndexBySocket( socket )];
-		person.name = data.name;
-
-		notifyEveryone( {message: person.name + ' has joined', name: 'System'} );
-	});
-
-	/*
-	 * Messagelistener, distributes the given message to all connected people
-	 */
-	socket.on('message', function( messagedata ) {
-		var person = people[findIndexBySocket( socket )];
-
-		notifyEveryone( {message: messagedata.message, name: person.name} );
-	});
-
-	/*
-	 * Disconnect logic
-	 */
-	socket.on('disconnect',function() {
-		// To prevent incorrect data in our people array, we need to remove the current person from the array
-		var index = findIndexBySocket( socket );
-		people.splice(index, 1);
-	});
-
-	/*
-	 * Finds the index of the person with the given socket
-	 */
-	function findIndexBySocket( socket ) {
-		for( var i = 0; i < people.length; i++ ) {
-			if( people[i].socket == socket ) {
-				return i;
-			}
-		}
-		return null;
-	}
-
-	/*
-	 * Sends the given message to all connected people
-	 */
-	function notifyEveryone( message ) {
-		for( var i = 0; i < people.length; i++ ) {
-			people[i].socket.emit('message', message);
-		}
-	}
-
-	/*
-	 * The object containing all the data required for chatting
-	 */
-	function Person( socket ) {
-		this.socket = socket;
-		this.name = 'Anonymous';
-	}
-});
-
+io.sockets.on('connection', chatlogic);
 
 // http server
-var static = require('node-static');
 
 //
 // Create a node-static server instance to serve the './clients' folder
@@ -74,9 +13,7 @@ var file = new(static.Server)('./');
 
 require('http').createServer(function (request, response) {
 	request.addListener('end', function () {
-		//
-		// Serve files!
-		//
+		// Serve the files to the client
 		file.serve(request, response);
 	});
 }).listen(8080);
